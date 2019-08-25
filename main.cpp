582ee8f20cf1b03eb38f6a5989d5067f45f3bc7a
Raúl Mosquera <raulv.mosquera@gmail.com>
2019-08-25 17:41:47 -0500

    jumping Mario finished

--- main.cpp
+++ main.cpp
@@ -1,8 +1,77 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	return EXIT_SUCCESS;
+pair<int, int> get_high_low_jumps(vector<int> heights) {
+    int num_high = 0;
+    int num_low = 0;
+    int jump = 0;
+    int negatives = 0;
+    int one_ocurrences = 0;
+
+    for (int i = 0; i < heights.size() - 1; i++) {
+
+        jump = heights.at(i + 1) - heights.at(i);
+
+        if (jump == 0)
+            continue;
+
+        if (jump < 0) {
+            negatives++;
+            num_low++;
+        } else if (jump == 1) {
+            one_ocurrences++;
+        } else {
+            num_high++;
+        }
+    }
+
+    if( negatives > 0 )
+        num_high = num_high + one_ocurrences;
+    else
+        num_low = num_low + one_ocurrences;
+
+    if (num_high == 0)
+        return make_pair(num_low, num_high);
+    else
+        return make_pair(num_high, num_low);
 }
 
+int main() {
+
+    int n;
+    vector<int> heights;
+    vector<pair<int, int>> results;
+    string input_line;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        fflush(stdin);  //FLUSHING STDIN
+        getline(cin, input_line);
+
+        getline(cin, input_line);
+
+        stringstream sep(input_line);
+        string height;
+
+        heights.clear();
+        while (getline(sep, height, ' ')) {
+            heights.push_back(stoi(height));
+        }
+
+        auto jumps = get_high_low_jumps(heights);
+
+        results.push_back(jumps);
+
+    }
+
+    for (int j = 0; j < results.size(); j++)
+        cout << "Case " << j + 1 << ": " << results.at(j).first << " " << results.at(j).second << endl;
+
+    return EXIT_SUCCESS;
+}
